84c4d6848a9599a3b70afb0159fedf5b3eb23231
elek8871 <elekmichelle@gmail.com>
2022-10-18 20:35:58 -0600

    completes deliverable

---
+++ challenges.py
@@ -0,0 +1,53 @@
+# Write a function that returns the number of vowels used in a string. Vowels are the characters 'a', 'e'
+
+# Example:
+
+# vowels('Hi There!') --> 3
+# vowels('Why do you ask?') --> 4
+# vowels('Why?') --> 0
+
+# string =input("enter your phrase: \n")
+
+def vowels(string):
+    #  define vowels using set{} -. set stores  multiple items into a single variable
+    vowels = set("aeiouAEIOU")
+    # init the vowel count at zero
+    count = 0
+    #  iterate through the string
+    for letters in string:
+    # if vowel is found return vowel+1
+        if letters in vowels:
+            count = count +1
+    print("No of vowels :", count)
+
+# vowels(string)
+
+
+
+# Given an integer n, return a string array answer (1-indexed) where:
+
+# answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+# answer[i] == "Fizz" if i is divisible by 3.
+# answer[i] == "Buzz" if i is divisible by 5.
+# answer[i] == i (as a string) if none of the above conditions are true.
+
+num = int(input("choose a number \n"))
+def fizzbizz(num):
+    # if num is div by 3 and 5 
+    if num % 3 == 0 and num % 5 == 0:
+    # return fizzbizz
+        print("fizzbizz")
+    #  if num div by 3 
+    elif num % 3 == 0:
+    #  return fizz
+        print("fizz")
+    #  if num div by 5
+    elif num % 5 == 0:
+        print ("bizz")
+    else:
+        print("number is not divisble by 3 or 5 boo")
+
+fizzbizz(num)
+
+
+
