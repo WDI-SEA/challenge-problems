8a498f9876def10767bd13d53858d1349e742f23
Adrian Rojo <rojoadrian16@gmail.com>
2022-10-19 12:52:30 -0500

    finished hw

--- answers.py
+++ answers.py
@@ -2,48 +2,78 @@
 
 # Example: 30 days from Monday is Wednesday.
 
-def reverse(word):
-    if len(word) == 0:
-        return word
-    else:
-        return reverse(word[1:]) + word[0]
+# def reverse(word):
+#     if len(word) == 0:
+#         return word
+#     else:
+#         return reverse(word[1:]) + word[0]
   
   
-print(reverse('computer'))
-print(reverse(reverse("computer")))
+# print(reverse('computer'))
+# print(reverse(reverse("computer")))
 
-import sys
 
-def minCoins(coins, m, target):
- 
-    # base case
-    if (target == 0):
-        return 0
- 
-    # Initialize result
-    
-    res = sys.maxsize
-     
-    # Try every coin that has smaller value than target
-    for i in range(0, m):
-        if (coins[i] <= target):
-            sub_res = minCoins(coins, m, target-coins[i])
- 
-            # Check for INT_MAX to avoid overflow and see if
-            # result can minimized
-            if (sub_res != sys.maxsize and sub_res + 1 < res):
-                res = sub_res + 1
- 
-    return res
- 
-coins = [9, 6, 5, 1]
-m = len(coins)
-target = 11
-
-print(minCoins(coins,m,target))
     
 
+# import sys
 
+# def sum(coins, length, target):
+    
+#     # Create a base case that will stop the function
+#     if(target == 0):
+#         return 0
+#     for i in range(0,length):
+#         if(coins[i] <= target):
+#            sub = sum(coins,length,target - coins[i])
+#            if(sub != target and sub + 1 <  length):
+#             sub = sub + 1
+#     return sub
+
+# coins = [25,25,25]
+# length = len(coins)
+# target = 40
+
+# sum(coins,length, target)
+
+
+
+# Direction:
+
+# Given an amount of change, determine the minimum number of coins required to make that change
+
+# Examples:
+# greedy(65) --> 4 `(2 quarters, 1 dime, 1 nickle)`
+# greedy(5) --> 1 `(1 nickle)`
+
+
+import math
+
+def greedy(change):
+    quarters = 25
+    dime = 10
+    nickle = 5
+    penny = 1
+
+    q = math.floor(change/quarters)
+    change = (change - q * quarters)
+    d = math.floor(change/dime)
+    change = (change - d * dime)
+    n = math.floor(change/nickle)
+    change = (change - n * nickle)
+    p = math.floor(change/penny)
+    change = (change - n * penny)
+
+    print(f'{q}: quarters, {d}: dimes, {n}: nickles, {p}: pennies')
+
+# PSEUDOCODE
+# 1. create variables for each value of cents
+# 2. divide the by the highest cent and return a floor number, round down
+# 3. subtract that value from the change
+# 4. continue steps 2 - 4
+
+# quarters = 25
+# print(math.floor(65/quarters))
+greedy(65)
 
 
 
