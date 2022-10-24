766137ae6e0c74c0d79ef5a4b8ced9ea1e271de6
Adrian Rojo <108231637+AdrianRRojo@users.noreply.github.com>
2022-10-24 18:50:27 -0500

    Update answers.py

--- answers.py
+++ answers.py
@@ -76,6 +76,51 @@ def greedy(change):
 greedy(65)
 
 
+# PART 2 QUESTIONS
+# 1)
+# Digital root is the recursive sum of all the digits in a number.
+# Given n, take the sum of the digits of n. If that value has more than one
+# digit, continue reducing in this way until a single-digit number is
+# produced. The input will be a non-negative integer.
+
+
+def digital root(n):
+x = str(n)
+r=0
+while len(x) > 1:
+r=0
+for i in range (len(x)):
+r = r + int(x[i])
+X = str(r)
+return r
 
     
+#  2)
+#  Given an array of integers nums, calculate the pivot index of this array.
 
+# The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+# If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+# Return the leftmost pivot index. If no such index exists, return -1
+
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        numsLen = len(nums)
+        if numsLen == 0: 
+            return -1
+        left = [0 for i in range(numsLen)]
+        right = [0 for i in range(numsLen)]
+        right[numsLen - 1] = nums[numsLen - 1]
+        for i in range(numsLen - 2, -1, -1):
+            right[i] = right[i + 1] + nums[i]
+        left[0] = nums[0]
+        if right[0] == left[0]:
+            return 0
+        for i in range(1, numsLen):
+            left[i] = left[i - 1] + nums[i]
+            if left[i] == right[i]:
+                return i
+        return -1
+        
