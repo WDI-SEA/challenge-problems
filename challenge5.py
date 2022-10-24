50682ba4851b787b66398c809085ffdbcbaf9bdb
syorn96 <syorn2014@gmail.com>
2022-10-23 23:09:25 -0700

    completes challenge 5

--- challenge5.py
+++ challenge5.py
@@ -1,2 +1,29 @@
 # Write a function that takes an array as a parameter and returns an array with the elements in a random order.
-import random
+import random, math
+arr = [1,2,3,4,5,6,7,8,9]
+
+def randomize(arr, x=None):
+    # take in array, randomly index
+    # after using each index, make the array smaller until there is none left (for loop?)
+    newArray = []
+    array = arr
+    x=len(array)
+    if x == 0:
+        return newArray
+    while x > 0:
+        y = random.randrange(x)
+        newArray.append(array[y])
+        del array[y]
+        x=len(array)
+        # array[y:y]
+        # randomize(array,x)
+    return newArray
+    
+
+print(randomize(arr))
+
+# x=len(arr)
+# y = random.randrange(x)
+# # print(x,y)
+# arr.pop(y)
+# print(arr)
