3b3181836c635cfba3ed248a0139e9b7a65f26e3
Heiner000 <garrett.heiner@gmail.com>
2023-05-16 22:18:45 -0700

    adds solve # 6

--- solves.py
+++ solves.py
@@ -75,9 +75,73 @@ def day_of_the_week(day, days_later, weeks_later):
     return days[new_day_index]
 
 #  Today is Sunday - what day of the week will it be in 100 days?
-print("sovle 3.1: ", day_of_the_week("Monday", 100, 0))
+print("solve 3.1: ", day_of_the_week("Monday", 100, 0))
 #  Today is Tuesday - What day of the week will it be in 4 weeks and 2 days?
-print("sovle 3.2: ", day_of_the_week("TUESDAY", 2, 4))
+print("solve 3.2: ", day_of_the_week("TUESDAY", 2, 4))
 #  Today is Friday - What day of the week will it be in 294 days?
-print("sovle 3.3: ", day_of_the_week("friday", 294, 0))
+print("solve 3.3: ", day_of_the_week("friday", 294, 0))
+
 #  Bonus: What month and day is it 73 days after October 31st 2018
+from datetime import datetime, timedelta
+def add_days(start_date, days_to_add):
+    # parse the start date into a datetime object
+    start_date = datetime.strptime(start_date, "%B %d %Y")
+    # calculate new date
+    new_date = start_date + timedelta(days=days_to_add)
+    # return the new date as a string w/ m, d, y
+    return new_date.strftime("%m %d, %Y")
+print("solve 3.bonus: ", add_days("October 31 2018", 73))
+
+'''
+# 4 -- Given a string, return a new string with the reversed order of characters
+reverse('apple') === 'leppa'
+reverse('hello') === 'olleh'
+reverse('Greetings!') === '!sgniteerG'
+'''
+def reverse(str):
+    return ''.join(reversed(str))
+
+print("solve 4: ", reverse('Greetings!'))
+
+
+'''
+# 5 -- Given a string, return true if the string is a palindrome or false if it is not. Do include spaces and punctuation in determining if the string is a palindrome.
+palindrome("abba") === true
+palindrome("abcdefg") === false
+'''
+def is_palindrome(str):
+    reversed_str = ''.join(reversed(str))
+    if str.lower() == reversed_str.lower():
+        return True
+    else:
+        return False
+    
+print("solve 5: ", is_palindrome("Abba"))
+
+'''
+# 6 -- Given a string, return the character that is most commonly used in the string
+maxChar("abcccccccd") === "c"
+maxChar("apple 1231111") === "1"
+'''
+def most_char(str):
+    char_dict = {}
+    for char in str:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    # find the key with the max value in char_dict
+    max_char = max(char_dict, key=char_dict.get)
+    return max_char
+
+print("solve 6: ", most_char("abbbbbcccccccd"))
+
+
+'''
+# 7 -- Given an array and chunk size, divide the array into many subarrays where each subarray is of length size
+chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+'''
